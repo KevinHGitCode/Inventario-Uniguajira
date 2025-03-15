8024cae19af9757d4de6bd78dd6449018be9af66
KevinHGitCode <kevinhafiddiazgarcia2003@gmail.com>
2025-03-15 15:08:14 -0500

    Implementar el diseño principal con una barra de navegación y una barra lateral responsivas; actualizar las rutas de los assets y los estilos.

---
+++ app/views/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inventario Uniguajira</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="assets/css/navbar.css">
+    <link rel="stylesheet" href="assets/css/sidebar.css">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/responsive/responsive.css">
+    
+</head>
+<body>
+    
+    <?php include 'app/views/navbar.html'; ?>
+
+    <?php include 'app/views/sidebar.html'; ?>
+
+    <main id="main">
+        
+    </main>
+
+    <script src="assets/js/scripts.js"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,1 +1,1 @@
-<?php include_once 'app/views/index.html';
+<?php include_once 'app/views/index.php'; ?>
