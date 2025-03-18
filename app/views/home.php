af37a662ac2993580efc31d635e3730901526fd8
ZapataMor <luiszapataa2017@gmail.com>
2025-03-18 17:22:22 -0500

    Añadido componente de lista de tareas en la vista home y estilos correspondientes en home.css. Creado archivo list-task.html para la estructura de las tareas.

--- app/views/home.php
+++ app/views/home.php
@@ -4,5 +4,12 @@ $userName = ctlHome::getUserNameById(1);
 ?>
 <h1>¡Bienvenido, <?php echo htmlspecialchars($userName); ?>!</h1>
 
-<!-- //TODO: Crear el componente de lista de tareas. -->
+<h2 class="tittle-list-task">Tareas pendientes</h2>
 
+<div class="container-list-task" style="border: 1px solid black;">
+    <?php for ($i = 0; $i < 2; $i++): ?>
+        <div class="contenedor-hijo">
+            <?php include __DIR__ . '/list-task.html'; ?>
+        </div>
+    <?php endfor; ?>
+</div>
