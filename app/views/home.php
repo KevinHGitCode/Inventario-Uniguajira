d1a4aa52ee4c60fe43fd81c3a1cb63ab44181a7b
KevinHGitCode <kevinhafiddiazgarcia2003@gmail.com>
2025-03-19 22:22:28 -0500

    Refactorizacion de la lista de tareas y actualizar los estilos en la vista de inicio.

--- app/views/home.php
+++ app/views/home.php
@@ -6,10 +6,12 @@ $userName = ctlHome::getUserNameById(1);
 
 <h2 class="tittle-list-task">Tareas pendientes</h2>
 
-<div class="container-list-task" style="border: 1px solid black;">
-    <?php for ($i = 0; $i < 2; $i++): ?>
+<!-- <div class="container-list-task" style="border: 1px solid black;">
+    < ?php for ($i = 0; $i < 2; $i++): ?>
         <div class="contenedor-hijo">
-            <?php include __DIR__ . '/list-task.html'; ?>
+            < ?php include __DIR__ . '/list-task.html'; ?>
         </div>
-    <?php endfor; ?>
-</div>
+    < ?php endfor; ?>
+</div> -->
+
+<?php include __DIR__ . '/list-task.html'; ?>
