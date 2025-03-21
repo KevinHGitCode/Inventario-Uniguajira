39508183c790e4d171586a28fd3234b106fe1b9f
KevinHGitCode <kevinhafiddiazgarcia2003@gmail.com>
2025-03-21 12:53:00 -0500

    Mejora del .htaccess; formateo del login; implementado un enrutador básico en index.php.

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-<?php include_once 'app/views/index.php'; ?>
+<?php 
+// include_once 'app/views/index.php'; 
+
+require 'app/controllers/Router.php';
+
+$router = new Router();
+
+$router->add('/', 'index.php');
+$router->add('/login', 'login.html');
+$router->add('/404', 'not-found.html');
+
+$requestUri = $_SERVER['REQUEST_URI'];
+$folderPath = dirname($_SERVER['SCRIPT_NAME']);
+$url = substr($requestUri, strlen($folderPath));
+
+$router->dispatch($url);
