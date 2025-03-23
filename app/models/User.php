016fb5b55a3636bcddd7310ad5c7aa16deac2693
KevinHGitCode <kevinhafiddiazgarcia2003@gmail.com>
2025-03-23 16:56:25 -0500

    Comentarios

--- app/models/User.php
+++ app/models/User.php
@@ -8,10 +8,14 @@ class User {
     }
 
     public function getById($id) {
+        // TODO: Cambiar * por los campos que se necesitan
         $stmt = $this->connection->prepare("SELECT * FROM usuarios WHERE id = ?");
         $stmt->bind_param("i", $id);
         $stmt->execute();
         $result = $stmt->get_result();
         return $result->fetch_assoc();
     }
+
+    // TODO: Agregar más métodos
+
 }
