bb803c484d382297de37b99284e698b834fff23a
KevinHGitCode <kevinhafiddiazgarcia2003@gmail.com>
2025-03-28 00:13:15 -0500

    cambios para el servidor

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php 
-// include_once 'app/views/index.php'; 
-
 require 'app/controllers/Router.php';
 
 $router = new Router();
@@ -9,14 +7,14 @@ $router->add('/', 'index.php');
 $router->add('/login', 'login.html');
 $router->add('/404', 'not-found.html');
 
-$requestUri = $_SERVER['REQUEST_URI'];
-$folderPath = dirname($_SERVER['SCRIPT_NAME']);
-$url = substr($requestUri, strlen($folderPath));
+// $requestUri = $_SERVER['REQUEST_URI'];
+// $folderPath = dirname($_SERVER['SCRIPT_NAME']);
+// $url = substr($requestUri, strlen($folderPath));
 
-// $router->dispatch($url);
-echo 'requestUri: '.$requestUri.'<br>';
-echo 'folderPath: '.$folderPath.'<br>';
-echo 'url: '.$url;
+$router->dispatch($url);
+// echo 'requestUri: '.$requestUri.'<br>';
+// echo 'folderPath: '.$folderPath.'<br>';
+// echo 'url: '.$url;
 
 // include 'app/controllers/Controller.class.php';
 // $ctl = new Controller();
