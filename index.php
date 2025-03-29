6d45b58c09a7ed32e4846b14ce395b44e36cda7b
KevinHGitCode <kevinhafiddiazgarcia2003@gmail.com>
2025-03-28 22:08:51 -0500

    Configuracion del index para dominio xampp

--- index.php
+++ index.php
@@ -8,13 +8,8 @@ $router->add('/login', 'login.html');
 $router->add('/404', 'not-found.html');
 
 $requestUri = $_SERVER['REQUEST_URI'];
-// $folderPath = dirname($_SERVER['SCRIPT_NAME']);
-// $url = substr($requestUri, strlen($folderPath));
-
 $router->dispatch($requestUri);
-// echo 'requestUri: '.$requestUri.'<br>';
-// echo 'folderPath: '.$folderPath.'<br>';
-// echo 'url: '.$url;
+
 
 // include 'app/controllers/Controller.class.php';
 // $ctl = new Controller();
