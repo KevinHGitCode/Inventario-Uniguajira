ed1a47caa1a50c6c1de71be2d3d532dfbd4e9ea6
KevinHGitCode <kevinhafiddiazgarcia2003@gmail.com>
2025-04-13 01:06:48 -0500

    guardar inventories.php

--- app/views/inventory/inventories.php
+++ app/views/inventory/inventories.php
@@ -1,22 +1,40 @@
-<h2>Lista de Inventarios</h2>
+<!-- En este h2 se insertara el nombre del grupo (inventory.js) -->
+<h2 id="group-name" class="text-secondary fs-5 fw-normal mb-4">dsdsd</h2>
 <button class="create-btn" onclick="cerrarGrupo()">Cerrar</button>
 
-<?php include 'app/views/inventory/searchInventory.html' ?>
+<div class="top-bar">
+    <div class="search-container">
+        <input
+            class="search-bar"
+            type="text"
+            placeholder="Buscar o agregar inventarios"
+        />
+        <i class="search-icon fas fa-search"></i>
+    </div>
+    <button id="btnCrear" class="create-btn">Crear</button>
+</div>
 
 <div class="bienes-grid">
     <?php if (isset($dataInventories)): ?>
+
+        <!-- Por cada inventario -->
         <?php foreach ($dataInventories as $inventory): ?>
             <div class="bien-card">
                 <div class="bien-info">
-                    <h3><?= htmlspecialchars($inventory['nombre']) ?></h3>
+                    <h3 id="inventory-name<?=$inventory['id']?>"><?= htmlspecialchars($inventory['nombre']) ?></h3>
                 </div>
                 <div class="actions">
-                    <button class="create-btn" onclick="abrirInventario(<?= htmlspecialchars($inventory['id']) ?>)">Abrir</button>
+                    <button
+                        class="create-btn"
+                        onclick="abrirInventario(<?= htmlspecialchars($inventory['id']) ?>)"
+                    >
+                        Abrir
+                    </button>
                 </div>
             </div>
         <?php endforeach; ?>
+
     <?php else: ?>
         <p>No hay inventarios disponibles.</p>
     <?php endif; ?>
 </div>
-
