84519f03fe823a8c46cc8af25f68cd0325ebb1bf
KevinHGitCode <kevinhafiddiazgarcia2003@gmail.com>
2025-04-16 14:01:33 -0500

    Se añadio el favicon de uniguajira en las paginas principales

--- app/views/index.php
+++ app/views/index.php
@@ -7,6 +7,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inventario Uniguajira</title>
+    <!-- icono de la pestaña del navegador -->
+    <link href="assets/images/favicon-uniguajira-32x32.webp" rel="icon" type="image/png" />
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
